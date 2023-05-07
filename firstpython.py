d925085320754bdd05d8c78a6be4c64ea19ef92b
Boootstraper <128927547+Boootstraper@users.noreply.github.com>
2023-05-07 22:40:02 +0530

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,1 +1,13 @@
-
+first = input("enter first number:")
+first= int(first)
+operator = input("enter operator (+,-,*,/) :")
+second = input("enter second number:")
+second = int(second)
+if operator == "+":
+    print(first + second)
+if operator == "-":
+    print(first - second)
+if operator == "*":
+    print(first * second)
+else:
+    print("invalid operation")
